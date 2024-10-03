0be4809d93e9c09fcf80e42f883e3d7ae6172498
Yoftahe Merkebu <yoftahemerkebu2@gmail.com>
2024-10-03 12:46:58 +0300

    added likned list page

---
+++ src/app/linked-list/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LinkedListComponent from '../components/linked-list/LinkedListComponent';
+
+const page = () => {
+  return (
+    <div>
+      <LinkedListComponent />
+    </div>
+  );
+};
+
+export default page;
